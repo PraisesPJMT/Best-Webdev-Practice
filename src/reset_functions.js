12fb00736c47c1bd40cd3c78d447bb76772b7c6a
PraisesPJMT <praisesmusa@outlook.com>
2022-08-16 10:14:00 +0100

    Fix hover bug on checkbox when new task is added

--- src/reset_functions.js
+++ src/reset_functions.js
@@ -3,13 +3,18 @@ const todayListBox = document.querySelector('.list');
 export const resetColors = () => {
   const listItems = document.querySelectorAll('li');
   const inputs = document.querySelectorAll('input');
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  const hover = document.createTextNode('.checkbox:hover { backgroundColor: #b0b0b0 }');
   listItems.forEach((item) => {
     item.style.backgroundColor = '#fff';
   });
   inputs.forEach((input) => {
     input.style.backgroundColor = '#fff';
   });
-};
+  checkboxes.forEach((checkbox) => {
+    checkbox.style = hover;
+  });
+}
 
 export const resetList = () => {
   todayListBox.querySelectorAll('.func').forEach((button) => {
